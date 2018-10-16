665c54408e413146e680d56ae1a35fbbd27f962b
Natalie Ruiz <natnatruiz@gmail.com>
2018-10-16 11:31:39 -0500

    Connect to square api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './component/map'
+import SquareAPI from "./API/"
 
 class App extends Component {
+  componentDidMount(){
+    console.log("mounted!")
+    SquareAPI.search({
+      near: "Miami, FL",
+      query: "vegan",
+      limit: 10
+    }).then(results => console.log(results))
+  }
+
+
   render() {
     return (
       <div className="App">
